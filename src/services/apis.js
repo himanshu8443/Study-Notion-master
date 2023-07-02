c035df9164edeeb705bd5617b096646704f3cd69
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-07-02 11:34:35 +0530

    enrolled courses error fix

--- src/services/apis.js
+++ src/services/apis.js
@@ -12,7 +12,7 @@ export const endpoints = {
 // PROFILE ENDPOINTS
 export const profileEndpoints = {
   GET_USER_DETAILS_API: BASE_URL + "/profile/getUserDetails",
-  GET_USER_ENROLLED_COURSES_API: "http://localhost:4000/api/v1/profile/getEnrolledCourses",
+  GET_USER_ENROLLED_COURSES_API: BASE_URL +"/profile/getEnrolledCourses",
 }
 
 // STUDENTS ENDPOINTS
