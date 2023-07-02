2501ed85b1c05e97ee14f2b04e95de2baf118fd3
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-07-02 11:17:41 +0530

    added cors origin

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(cors(
     {
-        origin: "http://localhost:3000",
+        origin: "https://study-notion-eta.vercel.app",
         credentials: true,
     }
 ));
