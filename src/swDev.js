4df92e0ced27b7f2d8fed2939b2bf6a1b031f0ac
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-07-05 11:59:30 +0530

    service worker

--- src/swDev.js
+++ src/swDev.js
@@ -1,5 +1,5 @@
 export default function swDev() {
-  let swUrl = `${process.env.PUBLIC_URL}/sw.js`
+  let swUrl = `%${process.env.PUBLIC_URL}%/sw.js`
   navigator.serviceWorker.register(swUrl).then((response) => {
     console.warn('response', response)
   })
