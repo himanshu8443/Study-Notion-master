37119dbb1c7ed719f25a30545dfe8f31a96a4018
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-07-06 11:59:40 +0530

    setEditCourse

--- src/Components/core/Dashboard/SidebarLink.jsx
+++ src/Components/core/Dashboard/SidebarLink.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import * as Icons from "react-icons/vsc"
 import { useDispatch } from 'react-redux';
 import { NavLink, matchPath, useLocation } from 'react-router-dom';
+import { setEditCourse } from '../../../slices/courseSlice';
 
 const SidebarLink = ({link, iconName}) => {
 
@@ -22,7 +23,11 @@ const SidebarLink = ({link, iconName}) => {
 
         
 
-        <div className='flex item-center  gap-x-2 flex-col md:flex-row'>
+        <div className='flex item-center  gap-x-2 flex-col md:flex-row' onClick={
+            () => {
+                dispatch(setEditCourse(false))
+            }
+        }>
 
             <Icon className="md:text-lg text-2xl" />
             <span className='hidden md:block'>{link.name}</span>
