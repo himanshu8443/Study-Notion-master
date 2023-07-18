a8ae8388f37c40bdbac129e25c5248be9debfedf
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-07-18 14:58:27 +0530

    progressbar

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -37,7 +37,8 @@ export function sendOtp(email, navigate) {
       navigate("/verify-email")
     } catch (error) {
       console.log("SENDOTP API ERROR............", error)
-      toast.error("Could Not Send OTP")
+      toast.error(error.response.data.message)
+      dispatch(setProgress(100));
     }
     dispatch(setLoading(false))
     // toast.dismiss(toastId)
