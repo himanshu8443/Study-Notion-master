1bf0e9423cef447a957e7c0f318920947477675d
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-07-18 15:24:00 +0530

    nodemailer res

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -30,7 +30,7 @@ async function sendVerificationEmail(email, otp) {
 			"Verification Email",
 			emailTemplate(otp)
 		);
-		console.log("Email sent successfully: ", mailResponse.response);
+		console.log("Email sent successfully: ", mailResponse);
 	} catch (error) {
 		console.log("Error occurred while sending email: ", error);
 		throw error;
